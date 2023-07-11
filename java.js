f45198b8ce558a86e4d1e9361998db368805d531
MagaliM1 <maguimedrano08@gmail.com>
2023-07-11 20:00:54 -0300

    q

---
+++ java.js
@@ -0,0 +1,72 @@
+//ejercicio 1
+function ParOImpar (number) {
+    if(number % 2 === 0) {
+        console.log (number + ' Es par')}
+    else { console.log (number + ' Es impar')}
+    }
+
+ParOImpar (10)
+
+// ejercicio 2
+function MayorMenor (nro1, nro2) {
+    if(nro1 > nro2) { 
+        console.log(nro1)}
+    else{ 
+        console.log(nro2);}   
+    if (nro1==nro2) {
+        console.log('Ambos son iguales');} 
+}
+
+MayorMenor (3,5)
+
+//ejercicio 3
+function Multiplo5 (numero) {
+    if (numero> 0 && numero == 5) {
+        console.log ('El numero ' + numero + ' es multiplo de 5')
+    }
+    else {
+        console.log ('El numero '+ numero + ' no es multiplo de 5')
+    }
+}
+
+Multiplo5 (10)
+
+// ejercicio 4
+let i=5
+for (let i=0; i<6; i++){
+    console.log (i);
+}
+
+//ejercicio 5
+var1='Lala';
+var2=3;
+for (let i=0; i<var2; i++){
+    console.log (var1);} 
+
+
+//ejercicio 6
+var myArray = ['Hola', 10 , 'Lucia']
+console.log (myArray)
+
+//ejercicio 7
+for (let i=0; i<10; i++){
+    if( i==5) {
+        continue;
+    }      
+    console.log(i)
+}
+
+// ejercicio 8
+
+
+
+
+
+
+
+
+
+
+
+
+
